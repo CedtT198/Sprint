43b26c69d5a42eecc4b0cc9cde5068e88697fb74
CedtT198 <cedrictiavina426@gmail.com>
2024-05-07 12:14:02 -0600

    Update FrontController.java

--- FrontController.java
+++ FrontController.java
@@ -1,4 +1,4 @@
-// package mg.prom16.controller;
+package mg.prom16.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(name = "FrontController", urlPatterns = {"/"})
+// @WebServlet(name = "FrontController", urlPatterns = {"/"})
 public class FrontController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
