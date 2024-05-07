5dca3e37202ac98568b5bc664f8b002f57257598
CedtT198 <cedrictiavina426@gmail.com>
2024-05-07 12:01:41 -0600

    Update FrontController.java

--- FrontController.java
+++ FrontController.java
@@ -1,24 +1,28 @@
-package prom16.controller;
+// package mg.prom16.controller;
 
-import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
-public class FormAchatServlet extends HttpServlet {
+@WebServlet(name = "FrontController", urlPatterns = {"/"})
+public class FrontController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        StringBuffer url = request.getRequestURL();
+        PrintWriter pw = response.getWriter();
+        pw.println(url.toString());
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
-
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
